e3c2356a4fa72a74e1213467d010d63c9f831990
KAPIL <95291600+59330kapil@users.noreply.github.com>
2024-02-15 18:53:05 +0530

    Add files via upload

---
+++ ServiceProviderAppApplication.java
@@ -0,0 +1,14 @@
+package com.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProviderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProviderAppApplication.class, args);
+	}
+	
+
+}
